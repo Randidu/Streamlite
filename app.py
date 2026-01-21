75765ff39cf48e96b29142f42647a65c33a2331e
Randidu <randidudamsith96@gmail.com>
2026-01-21 16:12:59 +0530

    steamlit application

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from transformers import pipeline
+
+st.set_page_config(page_title="Ai summarization Tool")
+
+@st.cache_resource
+def load_model():
+    return pipeline("summarization",model="facebook/bart/-large-cnn")
+
+summarizer_model= load_model
+
+st.title("Text Summarizer Tool ")
+col1,col2 = st.columns([1,2])
+
+with col1:
+    user_input=st.area_chart("enter text to summarizer", hight=200)
+    summarizer_button = st.button("Summarizer Text",type="primary")
+
+with col2:
+    st.markdown("Powered by C Clarke Institute Student")
+
+if summarizer_button and user_input:
+    with st.spinner("Summarizing....."):
+        result = summarizer_model(user_input)
+        summary_text= result[0]['summary_text']
+        st.markdown(summary_text)
+elif summarizer_button:
+    st.warning("please enter some text to summarize...")
+
+
