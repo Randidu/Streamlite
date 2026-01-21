71599d03989e9ae6fc07dd3a95e4fe7f5f75e8d9
Randidu <randidudamsith96@gmail.com>
2026-01-21 16:33:59 +0530

    commit

--- app.py
+++ app.py
@@ -9,22 +9,20 @@ def load_model():
 
 summarizer_model = load_model()
 
-st.title("Text Summarizer Tool")
-col1, col2 = st.columns([1, 2])
-
+st.title("Text Summarization Tool")
+col1, col2 = st.columns([2,1])
 with col1:
-    user_input = st.text_area("Enter text to summarize", height=200)
+    user_input =st.text_area("Enter text to summarize", height=200)
     summarizer_button = st.button("Summarize Text", type="primary")
 
 with col2:
-    st.markdown("Powered by C Clarke Institute Student")
+    st.markdown("Powered by C Clarke Institute Students")
 
-if summarizer_button:
-    if user_input.strip():
-        with st.spinner("Summarizing..."):
-            result = summarizer_model(user_input)
-            summary_text = result[0]['summary_text']
-            st.markdown("### Summary:")
-            st.markdown(summary_text)
-    else:
-        st.warning("Please enter some text to summarize...")
+if summarizer_button and user_input:
+    with st.spinner("Summarizing......"):
+        result = summarizer_model(user_input)
+        summary_text = result[0]['summary_text']
+        st.markdown(summary_text)
+
+elif summarizer_button:
+    st.warning("Please Enter some text to summarize")
